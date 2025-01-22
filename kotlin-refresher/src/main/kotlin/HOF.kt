bd274664db15befe34a77799c0496e4f071f48f0
Bandenawaz Bagwan <bandenawaz.bagwan@publicissapient.com>
2025-01-22 21:03:31 +0530

    Added HOF,Sealed Classes, Data Classes and other concepts

--- kotlin-refresher/src/main/kotlin/HOF.kt
+++ kotlin-refresher/src/main/kotlin/HOF.kt
@@ -3,6 +3,16 @@ class HOF {
     fun performOperation(x: Int, y: Int, operation: (Int, Int) -> Int): Int {
         return operation(x, y)
     }
+
+    //function to demonstrate extensions
+    //extension function to swap elements in a Mutable list
+
+}
+
+fun <T> MutableList<T>.swap(index1: Int, index2: Int){
+    val temp = this[index1]
+    this[index1] = this[index2]
+    this[index2] = temp
 }
 
 fun main(){
@@ -12,4 +22,11 @@ fun main(){
 
     println("Sum : $sum ")
     println("Product : $product")
+
+    val list = mutableListOf(1,2,3,4,5,6)
+    println("Original List : $list")
+
+    //Using the extension function to swap elements
+    list.swap(0,2)
+    println("List after swapping : $list")
 }
